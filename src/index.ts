de76ff407c01b73d2ed6ee5f3014d0b94bfd2b87
Ahmad Graham <ahmadgrahamdev@gmail.com>
2025-07-21 01:33:01 -0400

    refactor: enhance socket connection handling by adding room state synchronization

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ import dotenv from "dotenv";
 import app from "./config/app";
 import { createServer } from "http";
 import { Server as SocketIOServer } from "socket.io";
-import { setSocketServer } from "./webhooks/roomWebhooks";
+import { emitRoomEvent, setSocketServer } from "./webhooks/roomWebhooks";
+import { getRoom } from "./services/RoomService";
 
 dotenv.config();
 
@@ -22,8 +23,19 @@ function startServer() {
 
     io.on("connection", (socket) => {
         // Client should join a room by roomId after connecting
+        console.log(`New client connected: ${socket.id}`);
         socket.on("join_room", (roomId: string) => {
             socket.join(roomId);
+            // Send the current room state to just this socket
+            const room = getRoom(roomId);
+            if (!room) {
+                console.error(
+                    `Room with ID ${roomId} not found for socket ${socket.id}`
+                );
+                return;
+            }
+
+            emitRoomEvent(room, "sync");
         });
     });
 
