b951cd8e0f1931e2f18b6038e9eebfe2c9ac6017
Ahmad Graham <ahmadgrahamdev@gmail.com>
2025-07-21 00:03:51 -0400

    feat: refactor server setup to integrate Socket.IO for real-time communication

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,35 @@
 // Entry point for the Express server
 import dotenv from "dotenv";
 import app from "./config/app";
+import { createServer } from "http";
+import { Server as SocketIOServer } from "socket.io";
+import { setSocketServer } from "./webhooks/roomWebhooks";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
 function startServer() {
-    app.listen(PORT, () => {
+    const httpServer = createServer(app);
+    
+    const io = new SocketIOServer(httpServer, {
+        cors: {
+            origin: "*",
+        },
+    });
+
+    setSocketServer(io);
+
+    io.on("connection", (socket) => {
+        // Client should join a room by roomId after connecting
+        socket.on("join_room", (roomId: string) => {
+            socket.join(roomId);
+        });
+    });
+
+    httpServer.listen(PORT, () => {
         // eslint-disable-next-line no-console
-        console.log(`Server is running on port ${PORT}`);
+        console.log(`Server and Socket.io running on port ${PORT}`);
     });
 }
 
